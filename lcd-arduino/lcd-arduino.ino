41cecdb18a86f7ce10af776027c2dfbade349a22
Vinh Le Huu <lehuuvinh92@gmail.com>
2021-10-13 21:35:31 +0700

    Update lcd-arduino.ino

--- lcd-arduino/lcd-arduino.ino
+++ lcd-arduino/lcd-arduino.ino
@@ -4,7 +4,7 @@ void setup() {
   lcd.begin(16, 2);//Khởi tạo màn hình LCD và xác định kích thước màn hình LCD là 16x2
 }
 void loop() {
-  lcd.clear();//Xóa màn hình và đặt con trỏ về vị trí đầu tiên
+  lcd.clear();//Xóa màn hình và đặt con trỏ về vị trí đầu tiên (0, 0)
   lcd.setCursor(6,0);//Di chuyển con trỏ đến cột tương ứng
   lcd.print("HELLO");//Xuất ra màn hình từ vị trí con trỏ
   lcd.setCursor(5,1);
