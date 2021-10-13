7cbcc5fe890982ed1e9c780a94ff6d9ed79d8453
Vinh Le Huu <lehuuvinh92@gmail.com>
2021-10-13 21:21:41 +0700

    Update lcd-arduino.ino

--- lcd-arduino/lcd-arduino.ino
+++ lcd-arduino/lcd-arduino.ino
@@ -1,10 +1,10 @@
 #include <LiquidCrystal.h>//Khai báo thư viện
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);//Khai báo các chân RS, E, D4-D7 kết nối với Arduino
 void setup() {
-  lcd.begin(16, 2);//Kết nối LCD 16x2
+  lcd.begin(16, 2);//Khởi tạo màn hình LCD và xác định kích thước màn hình LCD là 16x2
 }
 void loop() {
-  lcd.clear();//Xóa màn hình
+  lcd.clear();//Xóa màn hình và đặt con trỏ về vị trí đầu tiên
   lcd.setCursor(6,0);//Di chuyển con trỏ đến cột tương ứng
   lcd.print("HELLO");//Xuất ra màn hình từ vị trí con trỏ
   lcd.setCursor(5,1);
